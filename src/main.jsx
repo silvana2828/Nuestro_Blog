fdfdceecd2a6832346e4c494c663ec875f0e95be
silvana2828 <silvanaxuluaga0@gmail.com>
2024-03-20 23:54:00 -0500

    Solucionando problemas de github

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
-// import { BrouserRouter } from "react-router-dom";
-=======
 import { BrowserRouter } from 'react-router-dom'
->>>>>>> 3d4624fe6a25498cb0a63ece900170dc6c1c259b
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
